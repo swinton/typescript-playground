a235885c1251e21f2585a3d6ac38521eccc949c5
Steve Winton <stevewinton@gmail.com>
2019-10-19 15:08:11 -0500

    Unlock TypeScript's Features in Vanilla JS with @ts-check and JSDoc from @johnlindquist on @eggheadio
    https://egghead.io/lessons/javascript-unlock-typescript-s-features-in-vanilla-js-with-ts-check-and-jsdoc

---
+++ ts-check-js-doc-example.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+// https://egghead.io/lessons/javascript-unlock-typescript-s-features-in-vanilla-js-with-ts-check-and-jsdoc
+
+/**
+ * @param {string} greeting
+ * @return {string}
+ */
+function greet(greeting) {
+  return greeting
+}
